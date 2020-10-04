f9aab794a0c3e590cd8dc05da8e7616472ce9f4f
dedekm <dedekmm@gmail.com>
2020-10-04 16:41:54 +0200

    add loop animation & random colors

--- src/Loop.gd
+++ src/Loop.gd
@@ -1,4 +1,14 @@
-extends Area
+extends Spatial
 
-func _on_Loop_body_entered(body: Node) -> void:
+var colors = [Color("#ccffcc"),
+              Color("#ffe599"),
+              Color("#fad7ff")]
+
+func _ready() -> void:
+  var material = SpatialMaterial.new()
+  material.albedo_color = colors[randi() % colors.size()]
+  material.flags_unshaded = true
+  $Torus001.set_surface_material(0, material)
+
+func _on_Area_body_entered(_body: Node) -> void:
   queue_free()
