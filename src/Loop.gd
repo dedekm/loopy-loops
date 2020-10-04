318d52f9624fcb6dac202de91741132df7280ba3
dedekm <dedekmm@gmail.com>
2020-10-04 20:43:00 +0200

    add loop particles

--- src/Loop.gd
+++ src/Loop.gd
@@ -1,14 +1,26 @@
 extends Spatial
 
-var colors = [Color("#ccffcc"),
-              Color("#ffe599"),
-              Color("#fad7ff")]
+const colors = [Color("#ccffcc"),
+                Color("#ffe599"),
+                Color("#fad7ff")]
 
-func _ready() -> void:
+func _init() -> void:
   var material = SpatialMaterial.new()
   material.albedo_color = colors[randi() % colors.size()]
   material.flags_unshaded = true
   $Torus001.set_surface_material(0, material)
 
 func _on_Area_body_entered(_body: Node) -> void:
-  queue_free()
+  if $Torus001.visible:
+    $CrunchParticles.mesh = $CrunchParticles.mesh.duplicate()
+    $CrunchParticles.mesh.surface_set_material(0, $Torus001.get_surface_material(0))
+    
+    $CrunchParticles.set_emitting(true)
+    $Torus001.visible = false
+    
+    var timer = Timer.new()
+    timer.autostart = true
+    timer.wait_time = 1
+    timer.one_shot = true
+    timer.connect("timeout", self, "queue_free")
+    add_child(timer)
