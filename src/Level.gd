d174a9f0fbec4e0a12fdb5a3338826c48bb6dfc5
dedekm <dedekmm@gmail.com>
2020-10-05 18:27:51 +0200

    add loop generating

--- src/Level.gd
+++ src/Level.gd
@@ -1,4 +1,26 @@
 extends Spatial
 
+var loop_timer: Timer
+
 func _init() -> void:
   randomize()
+
+func _ready() -> void:
+  loop_timer = Timer.new()
+  loop_timer.autostart = true
+  loop_timer.wait_time = 3
+  loop_timer.connect("timeout", self, "generate_loop")
+  add_child(loop_timer)
+  
+func generate_loop() -> void:
+  var loop_scene = load("res://src/Loop.tscn")
+  var loop_instance = loop_scene.instance()
+  loop_instance.set_name("Loop" + str(randi()))
+
+  var rng = RandomNumberGenerator.new()
+  rng.randomize()
+  loop_instance.translation.y = 1
+  loop_instance.translation.x = rng.randi_range(-9, 9)
+  loop_instance.translation.z = rng.randi_range(-9, 9)
+
+  add_child(loop_instance)
